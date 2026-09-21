492e079256c80a03d426138aeb75047c18752f8e
theakshatraj <live.akshatraj@gmail.com>
2026-09-22 01:51:41 +0530

    image upload fix changing appwrite hostname

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "cloud.appwrite.io",
+        hostname: "sgp.cloud.appwrite.io",
       },
     ],
   },
